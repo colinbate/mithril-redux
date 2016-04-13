d5bcfc38b030f9d2ba0b34db68940f3a68e8d310
Julien Bisconti <julien.bisconti@gmail.com>
2016-04-13 14:43:00 +0200

    functional style

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
+import _ from 'lodash/fp';
+
 class _Provider {
-  init(store, mithril, Component) {
-    this.store = store;
-    this.mithril = mithril;
-    const comp = typeof Component === 'function' ? new Component() : Component;
-    return comp;
-  }
+	init( store, mithril, Component ) {
+		this.store = store; 		//eslint-disable-line
+		this.mithril = mithril; //eslint-disable-line
+		const comp = _.isFunction( Component ) ? new Component() : Component;
+		return comp;
+	}
 }
 
 export const Provider = new _Provider;
 
+function actionMapper( actions, dispatch ) {
 
-function wrapView(comp, actionMap) {
-  const origView = comp.view;
-  comp.view = (ctrl, ...args) => {
-    let nc = {...ctrl, ...actionMap};
-    return origView(nc, ...args);
-  };
+	if ( _.isFunction( actions ) ) {
+		return actions( dispatch );
+	} else if ( _.isObject( actions ) ) {
+		return _.flow(
+			_.keys,
+			_.filter( _.isFunction ),
+			_.map( mappedAction => ( ...factoryArgs ) => ( ...args ) => dispatch( mappedAction( ...factoryArgs, ...args ) ) )
+		)( actions );
+	}
 }
 
-export const connect = (selector, actions) => (Component) => {
-  return {
-    view (controller, props, children) {
-      const {dispatch, getState} = Provider.store;
-      let actionMap = {};
-      if (typeof actions === 'function') {
-        actionMap = actions(dispatch);
-      } else if (typeof actions === 'object') {;
-        for (let k of Object.keys(actions)) {
-          if (typeof actions[k] === 'function') {
-            actionMap[k] = (...factoryArgs) => (...args) => dispatch(actions[k](...factoryArgs, ...args))
-          }
-        }
-      }
-      const state = selector(getState());
-      const comp = typeof Component === 'function' ? new Component() : Component;
-      wrapView(comp, actionMap);
-      return Provider.mithril.component(comp, {dispatch, ...state, ...actionMap}, children);
-    }
-  };
+export const connect = ( selector, actions ) => ( Component ) => {
+	return {
+		view( controller, props, children ) {
+			const { dispatch, getState } = Provider.store;
+
+			const state = selector( getState() );
+			const comp = _.isFunction( Component ) ? new Component() : Component;
+			const actionMap = actionMapper( actions, dispatch );
+			const origView = comp.view;
+			const c = {
+				...comp,
+				view: ( ctrl, ...args ) => origView( { ...ctrl, ...actionMap }, ...args )
+			};
+			return Provider.mithril.component( c, { dispatch, ...state, ...actionMap }, children );
+		}
+	};
 };
 
-export const redrawMiddleware = (store) => (next) => (action) => {
-  next(action);
-  if (action.meta.redraw && Provider.mithril) {
-    Provider.mithril.redraw();
-  }
+export const redrawMiddleware = () => ( next ) => ( action ) => {
+	next( action );
+	if ( action.meta.redraw && Provider.mithril ) {
+		Provider.mithril.redraw();
+	}
 };
